e03e89dad7841cb8f07bcbecaab68378d9ca4dc2
Collin Fingar <fingac@rpi.edu>
2015-11-23 15:32:57 -0500

    Player can move left and right to limits and shoot. Shots destroy enemies.

---
+++ DecisionMaking_Vanderzee_Fingar/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = .05f;
+    public float minX = -3f;
+    public float maxX = 3f;
+
+    private bool goingLeft = false;
+    private bool goingRight = false;
+
+    public Transform shot;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            goingLeft = true;
+            goingRight = false;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            goingLeft = false;
+            goingRight = true;
+        }
+        else {
+            goingLeft = false;
+            goingRight = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            shoot();
+        }
+
+        move();
+
+	}
+
+    void move() {
+        if (goingLeft && transform.position.x > minX)
+        {
+            Vector2 position = transform.position;
+            transform.position = new Vector2(position.x - speed, position.y);
+        }
+        else if (goingRight && transform.position.x < maxX) {
+            Vector2 position = transform.position;
+            transform.position = new Vector2(position.x + speed, position.y);
+        }
+    }
+
+    void shoot() {
+        Instantiate(shot, transform.position, Quaternion.identity);
+    }
+
+
+}
---
+++ DecisionMaking_Vanderzee_Fingar/Assets/Scripts/ShotController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotController : MonoBehaviour {
+
+    public float speed = .05f;
+    public float maxHeight = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y > maxHeight) {
+            Destroy(gameObject);
+        }
+
+        Vector2 position = transform.position;
+        transform.position = new Vector2(position.x, position.y + speed);
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "alien") {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        
+    }
+}
